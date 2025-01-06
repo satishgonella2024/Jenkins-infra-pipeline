a0d599e6a085947ce615b596a8c27b693c7738b5
Subrahmanya Gonella <satishgonella2024@gmail.com>
2025-01-06 12:53:25 +0000

    Successfully added S3 bucket and public access policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "example" {
   bucket = "jenkins-infra-pipeline-example-london"
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket                  = aws_s3_bucket.example.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "example_policy" {
   bucket = aws_s3_bucket.example.id
   policy = jsonencode({
