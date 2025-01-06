71af9a3f3b8b2da30dfe72b155fab4e5715129a4
Subrahmanya Gonella <satishgonella2024@gmail.com>
2025-01-06 14:32:07 +0000

    Update Jenkinsfile to run Terratest before Terraform apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,6 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example_acl" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket                  = aws_s3_bucket.example.id
   block_public_acls       = true
