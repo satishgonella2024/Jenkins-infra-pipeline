2b448859e66acdf4bbedbce113f90dcc0d0ce26a
Subrahmanya Gonella <satishgonella2024@gmail.com>
2025-01-06 12:41:09 +0000

    Remove ACL configuration to support Bucket Owner Enforced

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
 resource "aws_s3_bucket" "example" {
   bucket = "jenkins-infra-pipeline-example-london"
 }
+
+resource "aws_s3_bucket_policy" "example_policy" {
+  bucket = aws_s3_bucket.example.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.example.arn}/*"
+      }
+    ]
+  })
+}
