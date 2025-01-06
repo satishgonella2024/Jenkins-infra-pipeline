5dc1cba667ce518a3793b8580cdb6c18b88bbf39
Subrahmanya Gonella <satishgonella2024@gmail.com>
2025-01-06 12:37:36 +0000

    Refactor S3 bucket ACL configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "jenkins-infra-pipeline-example"
+  bucket = "jenkins-infra-pipeline-example-london"
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
